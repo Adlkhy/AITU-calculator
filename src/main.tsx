17c860f90e941faf4963dbe21969c56cf991df30
Adlkhy <ersinadilhan.51@gmail.com>
2026-01-28 12:15:16 +0500

    Added vercel's speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { UserProvider } from './contexts/UserContext.tsx'
 import { GeminiProvider } from './contexts/GeminiContext.tsx'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const GA_ID = import.meta.env.VITE_GA_ID; 
 
@@ -30,6 +31,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GeminiProvider>
       <UserProvider>
+        <SpeedInsights />
         <App />
       </UserProvider>
     </GeminiProvider>
